493b69ee7f5719a3064d271f3157ccc9d39139c9
yota <youtai7777@gmail.com>
2023-11-04 00:48:46 +0900

    added entry asm

--- src/kernel/entry.rs
+++ src/kernel/entry.rs
@@ -7,6 +7,12 @@ use core::arch::asm;
 pub unsafe extern "C" fn _entry() -> ! {
     asm!(
       "la sp, STACK0",
+      "li a0, 4096 * {ssz}",
+      "csrr a1, mhartid",
+      "addi a1, a1, 1",
+      "mul a0, a0, a1",
+      "add sp, sp, a0",
+      ssz = const STACK_PAGE_NUM,
     );
 
     start();
